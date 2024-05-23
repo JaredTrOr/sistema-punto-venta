c6eb9b93dfedb0bb11ac542d436a3c66df6e3c3b
JaredTrOr <trujillojaredalexander@gmail.com>
2024-05-23 10:55:00 -0600

    Se realizó el diseño de la tabla del carrito, se realizaron las funcionalidades de agregar producto, eliminarlo y el cálculo de la cantidad de productos y el total

--- src/app/pages/empleados/ventas/ventas.component.ts
+++ src/app/pages/empleados/ventas/ventas.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Producto } from '../../../models/ProductoEjemplo';
 
+interface VentaProducto {
+  cantidad: number;
+  producto: Producto;
+}
+
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
@@ -8,7 +13,7 @@ import { Producto } from '../../../models/ProductoEjemplo';
 })
 export class VentasComponent {
   productos: Producto[] = [];
-  carritoProductos: Producto[] = [];
+  carritoProductos: VentaProducto[] = [];
 
   constructor() {}
 
@@ -17,99 +22,129 @@ export class VentasComponent {
       {
         id: 1,
         nombre: 'Producto 1',
-        precio: 100,
+        precio: 100.00,
         categoria: 'Categoria 1'
       },
       {
         id: 2,
         nombre: 'Producto 2',
-        precio: 200,
+        precio: 200.00,
         categoria: 'Categoria 2'
       },
       {
         id: 3,
         nombre: 'Producto 3',
-        precio: 300,
+        precio: 300.00,
         categoria: 'Categoria 3'
       },
       {
         id: 4,
         nombre: 'Producto 4',
-        precio: 150,
+        precio: 150.00,
         categoria: 'Categoria 1'
       },
       {
         id: 5,
         nombre: 'Producto 5',
-        precio: 250,
+        precio: 250.00,
         categoria: 'Categoria 2'
       },
       {
         id: 6,
         nombre: 'Producto 6',
-        precio: 350,
+        precio: 350.00,
         categoria: 'Categoria 3'
       },
       {
         id: 7,
         nombre: 'Producto 7',
-        precio: 175,
+        precio: 175.00,
         categoria: 'Categoria 1'
       },
       {
         id: 8,
         nombre: 'Producto 8',
-        precio: 275,
+        precio: 275.00,
         categoria: 'Categoria 2'
       },
       {
         id: 9,
         nombre: 'Producto 9',
-        precio: 375,
+        precio: 375.00,
         categoria: 'Categoria 3'
       },
       {
         id: 10,
         nombre: 'Producto 10',
-        precio: 200,
+        precio: 200.00,
         categoria: 'Categoria 1'
       },
       {
         id: 11,
         nombre: 'Producto 11',
-        precio: 300,
+        precio: 300.00,
         categoria: 'Categoria 2'
       },
       {
         id: 12,
         nombre: 'Producto 12',
-        precio: 400,
+        precio: 400.00,
         categoria: 'Categoria 3'
       },
       {
         id: 13,
         nombre: 'Producto 13',
-        precio: 225,
+        precio: 225.00,
         categoria: 'Categoria 1'
       },
       {
         id: 14,
         nombre: 'Producto 14',
-        precio: 325,
+        precio: 325.00,
         categoria: 'Categoria 2'
       },
       {
         id: 15,
         nombre: 'Producto 15',
-        precio: 425,
+        precio: 425.00,
         categoria: 'Categoria 3'
       }
   ];
   }
 
   agregarProducto(producto: Producto) {
-    this.carritoProductos.push(producto);
+
+    const productoEnCarrito = this.carritoProductos.find(productoCarrito => productoCarrito.producto.id === producto.id);
+
+    if(productoEnCarrito) {
+      productoEnCarrito.cantidad++;
+      return;
+    }
+
+    this.carritoProductos.push({
+      cantidad: 1,
+      producto
+    });
   }
 
+  eliminarProducto(productoId: number) {
+    const productoEnCarrito = this.carritoProductos.find(productoCarrito => productoCarrito.producto.id === productoId);
+
+    const indexOfProductoEnCarrito = this.carritoProductos.indexOf(productoEnCarrito!);
+    this.carritoProductos.splice(indexOfProductoEnCarrito, 1);
+  }
+
+  getTotalProductos() {
+    return this.carritoProductos.reduce((totalProductos, producto) => totalProductos + producto.cantidad, 0);
+  }
+
+  getTotal() {
+    let total = 0;
+    for (const producto of this.carritoProductos) {
+      total += producto.producto.precio * producto.cantidad;
+    }
+
+    return total;
+  }
 
 }
