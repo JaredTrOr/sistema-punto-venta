aa16e58784c45d68838b2dc9d3898b6683d92139
JaredTrOr <trujillojaredalexander@gmail.com>
2024-05-31 08:49:57 -0600

    Creación de una clase para obtener la hora y la fecha

---
+++ src/app/utils/tiempo.ts
@@ -0,0 +1,33 @@
+export class Tiempo {
+    constructor() {}
+
+    getHora(): string {
+        const date = new Date();
+        let hora = date.getHours();
+        let minutos = date.getMinutes();
+    
+        let horaString: string;
+        let minutosString: string;
+    
+        horaString = hora < 10 ? '0' + hora : hora.toString();
+        minutosString = minutos < 10 ? '0' + minutos : minutos.toString();
+    
+        return `${horaString}:${minutosString}`;
+    }
+    
+    getDate(): string {
+        const date = new Date();
+        let dia = date.getDate();
+        let mes = date.getMonth() + 1;
+        let anio = date.getFullYear();
+    
+        let diaString: string;
+        let mesString: string;
+    
+        diaString = dia < 10 ? '0' + dia : dia.toString();
+        mesString = mes < 10 ? '0' + mes : mes.toString();
+    
+        return `${diaString}/${mesString}/${anio}`;
+    }
+}
+
