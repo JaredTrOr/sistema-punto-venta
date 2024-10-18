99ad6ca73142a5d671cb341c4562c74d8a74bfbe
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-18 10:32:19 -0600

    Modificación de diseño de navbar de administrador

--- src/app/components/navbar-admin/navbar-admin.component.ts
+++ src/app/components/navbar-admin/navbar-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-admin',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class NavbarAdminComponent {
 
+  isAccordionOpen = true;
+
+  constructor(private router: Router) {}
+
+  toggleAccordion() {
+    this.isAccordionOpen = !this.isAccordionOpen;
+  }
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
